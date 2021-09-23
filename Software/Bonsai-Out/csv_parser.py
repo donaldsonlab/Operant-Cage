0b6904ffc8a30ea74a94070cdfebd7996838cf4a
Ryan Cameron <ryca1116@colorado.edu>
2021-09-23 12:39:02 -0600

    Updated csv parser

--- Software/Bonsai-Out/csv_parser.py
+++ Software/Bonsai-Out/csv_parser.py
@@ -1,6 +1,6 @@
 """ 
 Date Created : 9/9/2021
-Date Modified: 9/9/2021
+Date Modified: 9/23/2021
 Author: Ryan Cameron, David Protter, Sarah ???
 Description: This script is for post-processing the csv file that bonsai outputs for the fiber photometry. The purpose of this is to put the names of the events back into the data file that bonsai outputs.
 Property of Donaldson Lab, University of Colorado Boulder, PI Zoe Donaldson
@@ -10,7 +10,8 @@ Code located at - https://github.com/donaldsonlab/Operant-Cage
 # Imports
 import numpy as np
 import pandas as pd
-import time
+import time as tm
+import datetime as dt
 import sys
 
 # Parser
@@ -30,16 +31,41 @@ pulses = list(comms.loc[:]['pulse length (ms)'])
 
 # For each numeric value that is tagged correlate it to an event
 dataFile = 'Software//Bonsai-Out//data.csv'
+outputFile = 'Software//Bonsai-Out//finalData.csv'
 data = pd.read_csv(dataFile)
 vals = data.loc[:]['Value']
+times = data.loc[:]['Timestamp']
 newNames = []
+newTimes = []
 for iVal in range(len(vals)): # Loop through each value
+    # Get individual Values
     val = vals[iVal]
+    time = times[iVal]
+
+    # Create Event Name
     index = pulses.index(val) # Find the index of the value
     name = comms.loc[index]['command'] # Get the command name
+
+    # Create time since start of day in milliseconds
+    time = time.split('T') # Splits the date and the time
+    date = time[0]
+    time = time[1] # Get the time with utc offset
+    utcOff = "-" + time.split('-')[1]
+    time = time.split('-')[0]
+    # Convert the time to a time vector
+    timeVec = time.split(':')
+    # Convert to milliseconds
+    timeVec = list(map(float, timeVec))
+    timeVec[0] = timeVec[0]*60*60*1000
+    timeVec[1] = timeVec[1]*60*1000
+    timeVec[2] = timeVec[2]*1000
+    time = sum(timeVec)
+
     # Replace the number with the name
     newNames.append(name)
+    newTimes.append(time)
 
 # Save the file
 data.loc[:, 'Value'] = newNames
-data.to_csv(dataFile, index = False)
+data['Time in ms'] = newTimes
+data.to_csv(outputFile, index = False)
